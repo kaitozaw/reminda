1ab16a9d5ab8ea746539fbc151a6fe21f7924e0b
小澤海人 <kaitoozawa@ozawakaijins-MacBook-Pro.local>
2025-07-12 18:51:50 +1000

    Modify config.py

--- config.py
+++ config.py
@@ -5,16 +5,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "fallback-key")
-    if os.getenv("DATABASE_URL"):
-        SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
-    else:
-        POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
-        POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "")
-        POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
-        POSTGRES_DB = os.getenv("POSTGRES_DB", "reminda")
-        SQLALCHEMY_DATABASE_URI = (
-            f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/{POSTGRES_DB}"
-        )
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_COOKIE_SAMESITE = 'Lax'
     SESSION_COOKIE_SECURE = False
