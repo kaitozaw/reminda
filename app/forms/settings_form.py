2a9205d64925d00b5cdb532df9a8b4a42ada615a
小澤海人 <kaitoozawa@ozawakaijins-MacBook-Pro.local>
2025-07-14 23:32:00 +1000

    fix: handle 0 value in settings_form

--- app/forms/settings_form.py
+++ app/forms/settings_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, StringField, IntegerField, TextAreaField, SelectField, SubmitField
-from wtforms.validators import DataRequired, NumberRange, Length
+from wtforms.validators import DataRequired, InputRequired, NumberRange, Length
 
 TIMEZONE_CHOICES = [
     ("Etc/UTC", "UTC"),
@@ -57,9 +57,9 @@ TIMEZONE_CHOICES = [
 
 class SettingsForm(FlaskForm):
     reminder_activate = BooleanField("Activate Reminder")
-    reminder_timezone = SelectField("Reminder Timezone", choices=TIMEZONE_CHOICES, validators=[DataRequired()])
-    reminder_offset = SelectField("Reminder Timing", choices=[("1", "1 day before"), ("0", "Same day")], validators=[DataRequired()])
-    reminder_hour = IntegerField("Reminder Hour (0-23)", validators=[DataRequired(), NumberRange(min=0, max=23)])
-    email_subject = StringField("Email Subject", validators=[DataRequired(), Length(max=255)])
-    email_body = TextAreaField("Email Body", validators=[DataRequired()])
+    reminder_timezone = SelectField("Reminder Timezone", choices=TIMEZONE_CHOICES, validators=[InputRequired()])
+    reminder_offset = SelectField("Reminder Timing", choices=[("1", "1 day before"), ("0", "Same day")], validators=[InputRequired()])
+    reminder_hour = IntegerField("Reminder Hour (0-23)", validators=[InputRequired(), NumberRange(min=0, max=23)])
+    email_subject = StringField("Email Subject", validators=[InputRequired(), Length(max=255)])
+    email_body = TextAreaField("Email Body", validators=[InputRequired()])
     submit = SubmitField("Save")
